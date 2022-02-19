708e173deaceb0de235db9a61787b7a6eaf0c7b1
Sunah <csa4550@gmail.com>
2022-02-19 21:35:43 +0900

    2022-02-19

--- src/main/java/hello/hellospring/Service/MemberService.java
+++ src/main/java/hello/hellospring/Service/MemberService.java
@@ -3,14 +3,19 @@ package hello.hellospring.Service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
     //DI (test와 같은 인스턴스 사용)
+    //controller 의존성 주입
+    @Autowired
     public MemberService(MemberRepository memberRepository){ //외부에서 넣어지도록
         this.memberRepository = memberRepository;
     }
---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,18 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    // service의 기능을 다른 곳에서도 활용하므로(ex.주문시, 결제시) 객체를 공용으로 만드는것이 좋다 == new를 사용하지 말것
+    private final MemberService memberService;
+
+    // 공용 연결을 해주는것 - @Service 지정 필요
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
